6335c521a10736988db2f9cfe7d4753fdfd383b0
thalari sai teja <thalarisaiteja@MacBook-Air.local>
2024-06-03 15:36:25 -0500

    update to date

--- resources/js/Pages/Checkout.vue
+++ resources/js/Pages/Checkout.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Inertia, useForm } from '@inertiajs/inertia-vue3';
+import { Head, Inertia, useForm } from '@inertiajs/vue3';
 import { toRefs, ref, computed, onMounted, onBeforeMount } from 'vue'
 
 import { useCartStore } from '@/store/cart'
